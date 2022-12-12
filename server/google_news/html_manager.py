3fefb30b3d19fb022d9d5f10f813033eacc730f2
Jedidiah-Miller <jedidiah.a.miller@gmail.com>
2022-12-11 16:33:23 -0800

    get html, get urls from html

---
+++ server/google_news/html_manager.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from bs4 import element
+
+
+class HtmlManager:
+
+
+    def __init__(self):
+        print(self.__str__, ': INITIALIZED')
+
+
+    def get_soup(self, text, features = "html.parser") -> BeautifulSoup:
+        return BeautifulSoup(text, features)
+
+
+    def get_element_with_attributes(self, text, name, attributes: dict) -> element.PageElement:
+        soup = self.get_soup(text)
+        return soup.find(name, attrs=attributes).find_all
+
+
+    def get_all_elements_with_attributes(self, text, name, attributes: dict) -> element.ResultSet:
+        soup = self.get_soup(text)
+        return soup.find_all(name, attrs=attributes)
+
+
+    def get_all_urls(self, elements: element.ResultSet) -> list:
+        '''
+        returns all URLs from the given element, URLs are extracted from a tags
+        '''
+        urls = []
+
+        e: element.PageElement
+
+        for e in elements:
+            a_links = e.find_all('a')
+            for a in a_links:
+                urls.append(a.get('href'))
+
+        return urls
+
+
